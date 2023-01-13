68299b8563410659cfaeec7ccec94b71b433b346
heltonbase <111578214+heltonbase@users.noreply.github.com>
2023-01-13 15:30:27 -0300

    feat(main): style guides

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import React from 'react'
+import GlobalStyles from './styles/global.styles'
+import Header from './components/Header'
+
 function App() {
-    return (
-        <div>
-        </div>
-    )
+  return (
+    <>
+      <Header />
+      <GlobalStyles />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 200px;
+  align-items: center;
+  justify-content: center;
+
+  background: var(--gray-700);
+`
+
+export const Logo = styled.img``
